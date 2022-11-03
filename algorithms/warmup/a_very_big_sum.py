1a4510ff19f2e5bca1d417544b9c944ec7748395
Oscar <oorellana30.95@gmail.com>
2022-11-02 20:44:59 -0500

    Add a very big sum and simple array sum

---
+++ algorithms/warmup/a_very_big_sum.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+from collections import Counter
+
+
+#
+# Complete the 'camelcase' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def a_very_big_sum(ar):
+    return sum(ar)
+
+
+if __name__ == '__main__':
+    print(a_very_big_sum([1000000001, 1000000001, 1000000001]))
---
+++ algorithms/warmup/simple_array_sum.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+
+#
+# Complete the 'camelcase' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def simple_array_sum(ar):
+    return sum(ar)
+
+
+if __name__ == '__main__':
+    print(simple_array_sum([10, 11, 12]))
