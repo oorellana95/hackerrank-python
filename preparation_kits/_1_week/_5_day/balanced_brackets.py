25a163a3b5ef7227c6041a06dc4000209b7193b4
oorellana95 <oorellana30.95@gmail.com>
2023-09-13 23:53:49 -0500

    add balanced_brackets.py

--- preparation_kits/_1_week/_5_day/balanced_brackets.py
+++ preparation_kits/_1_week/_5_day/balanced_brackets.py
@@ -17,6 +17,20 @@ def isBalancedWeak(s):
     return "YES"
 
 
+def isBalanced(s):
+    if len(s) % 2 != 0:
+        "NO"
+
+    previous_length = 0
+    while previous_length != len(s):
+        previous_length = len(s)
+        s = s.replace('[]', '')
+        s = s.replace('{}', '')
+        s = s.replace('()', '')
+
+    return "YES" if len(s) == 0 else "NO"
+
+
 if __name__ == '__main__':
 
     t = 4
@@ -24,6 +38,6 @@ if __name__ == '__main__':
     for t_itr in range(t):
         s = _input[t_itr]
 
-        result = isBalancedWeak(s)
+        result = isBalanced(s)
 
         print(result)
